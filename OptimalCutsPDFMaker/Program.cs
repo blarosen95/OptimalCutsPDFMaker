347dcf8ebaeb8408dd1d007e454bdb74ef9ff654
Blake Rosenberg <barosenberg@bhmpc.com>
2021-09-18 12:38:50 -0400

    Initial Commit

---
+++ OptimalCutsPDFMaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OptimalCutsPDFMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
